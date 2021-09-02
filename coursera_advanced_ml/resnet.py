55f9f9b5676f731c98f5bd5c25dc40c3f7629c3d
someshfengde <someshfengde@gmail.com>
2021-09-02 22:15:05 +0530

    updating resnet file

--- coursera_advanced_ml/resnet.py
+++ coursera_advanced_ml/resnet.py
@@ -1,6 +1,9 @@
+import datetime
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+from tensorflow.keras import callbacks
+from tensorflow.python.ops.math_ops import DivideDelegateWithName
 
 
 class IdentityBlock(tf.keras.Model):
@@ -45,15 +48,79 @@ class Resnet(tf.keras.Model):
         return self.classifier(x)
 
 
-custom_model = Resnet(10)
-x = tf.random_normal_initializer()
-y = x(shape=(32, 28, 28, 1), dtype=tf.float32)
-z = x(shape=(32,), dtype=tf.float32)
+# custom_model = Resnet(10)
+# x = tf.random_normal_initializer()
+# y = x(shape=(32, 28, 28, 1), dtype=tf.float32)
+# z = x(shape=(32,), dtype=tf.float32)
 
-custom_model.compile(optimizer=tf.keras.optimizers.Adam(),
-                     loss=tf.keras.losses.sparse_categorical_crossentropy,
-                     metrics=['mae'])
+# custom_model.compile(optimizer=tf.keras.optimizers.Adam(),
+#                      loss=tf.keras.losses.sparse_categorical_crossentropy,
+#                      metrics=['mae'])
 
 
-# fitting the model
-cutsom_model_history = custom_model.fit(y, z, epochs=20)
+# # fitting the model
+# cutsom_model_history = custom_model.fit(y, z, epochs=20)
+
+
+# building new model with simpler layer
+class Mod(tf.keras.Model):
+    def __init__(self, units):
+        super(Mod, self).__init__()
+        self.units = units
+        self.d_lay = tf.keras.layers.Dense(units, activation='linear')
+
+    def call(self, inputs):
+        x = self.d_lay(inputs)
+        x = self.d_lay(x)
+        return x
+
+
+model = Mod(1)
+
+model.compile(loss=tf.keras.losses.mse,
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=['mae', tf.keras.metrics.RootMeanSquaredError()])
+
+# building custom loss function
+
+
+class Custom_callback(tf.keras.callbacks.Callback):
+    def on_train_batch_begin(self, batch, logs=None):
+        print(f'training batch {batch} begin at {datetime.datetime.now()}')
+        # return super().on_train_batch_begin(batch, logs=logs)
+
+    def on_train_batch_end(self, batch, logs=None):
+        print(f'training batch {batch} ends at {datetime.datetime.now()}')
+        # return super().on_train_batch_end(batch, logs=logs)
+
+
+my_custom_callback = Custom_callback()
+X_train = tf.constant(np.arange(0, 1000))
+y_train = tf.random.uniform(shape=(1000,))
+model.fit(X_train, y_train, batch_size=64, epochs=1, verbose=0,
+          callbacks=[my_custom_callback])
+
+
+# making detect overfitting callback
+
+class DetectOverFittingCallback(tf.keras.callbacks.Callback):
+    def __init__(self, threshold):
+        super(DetectOverFittingCallback, self).__init__()
+        self.threshold = threshold
+
+    def on_epoch_begin(self, epoch, logs='None'):
+        ratio = logs['val_loss']/logs['loss']
+        print(f'epoch {epoch} val_train loss raio = {ratio}')
+
+        if ratio > self.threshold:
+            print('Stopping training')
+            self.model.stop_training = True
+
+        # return super().on_epoch_begin(epoch, logs=logs)
+
+
+model.fit(X_train[:500], y_train[:500], validation_data=(X_train[500:], y_train[500:]), epochs=100,
+          callbacks=[DetectOverFittingCallback(1.3), my_custom_callback])
+
+
+# making Visual callback
